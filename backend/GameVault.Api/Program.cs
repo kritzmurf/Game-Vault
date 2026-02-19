e3529a89a205474c97cd4205d9f86542448c5797
Patrick Murphy <patmurf@live.com>
2026-02-19 16:16:56 -0600

    added dbconnectionfactory singleton to builder services in program.cs

--- backend/GameVault.Api/Program.cs
+++ backend/GameVault.Api/Program.cs
@@ -1,10 +1,20 @@
+using GameVault.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Services
 builder.Services.AddControllers();
+
 //Automatic REST API documentation for development only
 // See: https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//Set up database connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+
+builder.Services.AddSingleton(new DbConnectionFactory(connectionString));
 
 var app = builder.Build();
 
